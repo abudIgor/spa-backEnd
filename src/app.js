b719a15db236c0f31d558e5aefc5764dbc1d08e4
IGOR ABUD FORTES <igorfortes@ciandt.com>
2023-04-10 19:08:31 -0300

    test cors

--- src/app.js
+++ src/app.js
@@ -8,10 +8,12 @@ app.use(
         extended : true,
     })
 );
-
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
+    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+})
 
 const indexRouter = require("./routers/index");
 const leadRouter = require("./routers/lead");
