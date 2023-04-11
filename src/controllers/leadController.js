b95cbe85aafda5e50062b68687c3027203eea856
IGOR ABUD FORTES <igorfortes@ciandt.com>
2023-04-11 12:41:38 -0300

    teste

--- src/controllers/leadController.js
+++ src/controllers/leadController.js
@@ -4,7 +4,6 @@ const LeadService = require("../services/LeadService");
 exports.add = async (req,res) => {
    try {
       const createLead = await LeadService.addLead(req.body);
-      // if(req.method === 'OPTIONS') { return res.status(200).json(createLead) }
       res.status(201).json(createLead);
    } catch(error) {
         return res.status(500).json({error:error});
