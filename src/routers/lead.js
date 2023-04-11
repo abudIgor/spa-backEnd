97379cf1c25736a4d296c3dbb7cc010be25fb31c
IGOR ABUD FORTES <igorfortes@ciandt.com>
2023-04-11 14:11:18 -0300

    test lead cors

--- src/routers/lead.js
+++ src/routers/lead.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/leadController");
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 /*Post*/
 router.post("/",controller.add);
