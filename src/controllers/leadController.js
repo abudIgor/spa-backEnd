82054d22987625604ebc7ee4900ee0cda237544c
IGOR ABUD FORTES <igorfortes@ciandt.com>
2023-04-11 12:35:45 -0300

    useCors

--- src/controllers/leadController.js
+++ src/controllers/leadController.js
@@ -1,8 +1,10 @@
 const Todo = require("../models/Lead");
 const LeadService = require("../services/LeadService");
+const cors = require("cors");
 
 exports.add = async (req,res) => {
    try {
+      res.use(cors());
       const createLead = await LeadService.addLead(req.body);
       // if(req.method === 'OPTIONS') { return res.status(200).json(createLead) }
       res.status(201).json(createLead);
