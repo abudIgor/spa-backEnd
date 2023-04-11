a2488001a9301c2d7485b4360d3a7eec0fad2d44
IGOR ABUD FORTES <igorfortes@ciandt.com>
2023-04-11 15:04:42 -0300

    teste

--- src/api/leadController.js
+++ src/api/leadController.js
@@ -3,8 +3,8 @@ const LeadService = require("../services/LeadService");
 
 exports.add = async (req,res) => {
    try {
-      res.setHeader("Access-Control-Allow-Credentials", `true`);
-      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       const createLead = await LeadService.addLead(req.body);
       res.status(201).json(createLead);
    } catch(error) {
